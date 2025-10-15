632ecb8b401c731c232a0ba1a350a9fdf1b7fee9
ExpBuilder <102679788+ExpBuilder@users.noreply.github.com>
2025-10-15 22:45:07 +0000

    Added descriptions for logic and removed output text spacing issues.

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+// Joe Jiao
+// October 15, 2025
+// This program determines the user's perfect pet given their inputs of favorite color & season along with name
+// The selection for the perfect pet is determined by logic written on the rubric in chronological order
+
 import java.util.Scanner;
 
 public class Main {
@@ -12,58 +17,89 @@ public class Main {
         // Scanner definition
         Scanner scanner = new Scanner(System.in);
 
-        // Gets favorite color (of those available)
+        // Repeatedly asks user for their favorite color (of those available) until they make a valid entry
         while (true) {
+            // Asks user for favorite color
             System.out.println("Enter your favorite color (Red, blue, or green)");
             favColor = scanner.nextLine();
             favColor = (favColor.toLowerCase()).trim();
             
+
+            // Checks if user input is valid. If so, the loop will end. Else the user will be asked again
             if ((favColor.equals("red")) || (favColor.equals("blue")) || (favColor.equals("green"))){
                 break;
-            } else System.out.println("Invalid imput");
+            } else System.out.println("Invalid imput. Please try again.");
         }
 
-        // Gets favorite season
+        // Repeatedly asks user for their favorite season until they make a valid entry
         while (true) {
+            // Asks user for favorite season
             System.out.println("Enter your favorite season (Winter, Spring, Summer, or Autumn)");
             favSeason = scanner.nextLine();
             favSeason = (favSeason.toLowerCase()).trim();
             
+            // Checks if user input is valid. If so, the loop will end. Else the user will be asked again
             if ((favSeason.equals("winter")) || (favSeason.equals("spring")) || (favSeason.equals("summer")) || (favSeason.equals("autumn"))){
                 break;
-            } else System.out.println("Invalid imput");
+            } else System.out.println("Invalid imput. Please try again.");
         }
 
-        // Gets name
-        System.out.println("Enter your name");
-        name = scanner.nextLine();
+        // Repeatedly asks user for their name until they enter a valid name
+        boolean validName = true;
+        while (true) {
+            // Asks user for name
+            System.out.println("Enter your name (Without accents and other special characters)");
+            System.out.println("Periods, hyphens, and apostrophes are acceptable");
+            name = scanner.nextLine();
+
+            validName = true;
+
+            // Checks if user included invalid characters in their name
+            for (int i = 0; i < name.length() - 1; i++) {
+                if ("abcdefghijklmnopqrstuvwxyz.-\' ".indexOf((name.toLowerCase()).charAt(i)) == -1) {
+                    validName = false;
+                    break;
+                }
+            }
+
+            // Ends the infinite loop if the user input is valid
+            if (validName) break;
+            // Repeats the loop if user input is invalid
+            else System.out.println("Your input seems to contain invalid characters. Please try again");
+        }
+        // Determines if the user's (inputed) name begins with a vowel
         vowel = ("aeiouAEIOU".indexOf(name.substring(0,1)) != -1);
 
-        // Closes scanner 
+        // Closes scanner as it is no longer used (github gets mad because of reasource leak if I don't close it).
         scanner.close();
 
-        // Determines perfect pet
+        // Determines perfect pet based on user input
+        // Logic is done in chronological order of what was written on the rubric
         if (favColor.equals("blue")) {
             if (favSeason.equals("autumn")) pet = "alligator";
             else if (favSeason.equals("spring")) pet = "ostrich";
+
         } else if (favColor.equals("green")) {
             if (!vowel && (favSeason.equals("winter"))) pet = "giraffe";
             else if (!favSeason.equals("autumn")) pet = "dog";
+
         } else {
             if (vowel) pet = "panda";
             else pet = "porcupine";
+
         }
 
         if (favSeason.equals("summer") && !(pet.equals("dog") || pet.equals("panda") || pet.equals("porcupine"))) {
             pet = "pony";
         }
+
         if (!vowel && (favColor.equals("blue")) && !(favSeason.equals("summer") || favSeason.equals("autumn")) && !(pet.equals("ostrich"))) {
             pet = "axolotl";
         }
 
         if (pet.equals("placeholder")) pet = "rock";
 
-
-        System.out.println("Your perfect pet is a" + pet);
+        // Prints user's perfect pet
+        System.out.println("Your perfect pet is a " + pet);
     }
 }
